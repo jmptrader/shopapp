f139a674b1a62e39a5763e234a090dfb684e2b79
ever <cui_xl@163.com>
2017-03-05 22:24:29 +0800

    add page and component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+	<app-header></app-header>
     <router-view></router-view>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
+import Header from '@/components/head'
+import Footer from '@/components/footer'
 export default {
-  name: 'app'
+  components:{
+  	appHeader:Header,
+  	appFooter:Footer,
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import "../static/css/app.scss";
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,45 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+
+import App from '../app'
+import Login from '@/page/login'
+import Regist from '@/page/regist'
+import User from '@/page/user'
+import Cart from '@/page/cart'
+import Order from '@/page/order'
+import Index from '@/page/index'
+import Goods from '@/page/goods'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+const routes = [
+  {
+    path: '/',
+    component: App, // 默认页面
+    children: [
+    // 动态路由匹配
+      {path: '/login', name: 'login', component: Login},
+      {path: '/User/:id', name: 'user', component: User},
+      {path: '/regist', name: 'regist', component: Regist},
+      {path: '/cart', name: 'cart', component: Cart},
+      {path: '/order/:id', name: 'order', component: Order},
+      {path: '/index/:type', name: 'index', component: Index},
+      {path: '/goods/:id', name: 'goods', component: Goods},
+    ]
+  }
+]
+const router = new Router({
+  routes, // short for routes: routes
+  linkActiveClass: 'active',  // router-link的选中状态的class，也有一个默认的值
+  history: true
 })
+// router.beforeEach(function (to,from,next) {
+//   var userMsg = localStorage.getItem('userMsg')
+//   if(to.path === '/home'){
+//     if(!userMsg){
+//       next({ path: '/login' })
+//     }
+//   }
+//   next()
+// })
+export default router
