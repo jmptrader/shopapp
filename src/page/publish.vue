d82b13478662e50747c77d40e4abfe8bf1d11a65
cuixiaoli <cuixiaoli>
2017-03-13 18:02:37 +0800

    发布

--- src/page/publish.vue
+++ src/page/publish.vue
@@ -1,22 +1,48 @@
 <template>
-	<p>pulish</p>
+	<div class="app-publish">
+		<div class="ui-form ui-border-t">
+		    <form >
+		        
+		        <div class="ui-form-item ui-form-item-textarea ui-border-b">
+		            <label>
+		                发表文字
+		            </label>
+		            <textarea placeholder="input text"></textarea>
+		        </div>
+		    </form>
+
+		    <div class="ui-btn-wrap">
+			    <button class="ui-btn-lg">
+			        确定
+			    </button>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
-export default{
-	data(){
-		return{
-
-		}
-	},
-	created :function () {
-		this.$store.comm('changeIndexConf',{
-			isFooter: true,
-	        isHeader: true,
-	        isSearch: true,
-	        title: '发布'
-		})
-	}
+import axios from 'axios'
+import router from '../router'
+export default {
+  data () {
+    return {
+      avatar:'',
+      uname:'',
+    }
+  },
+  created: function () {
+      this.$store.commit('changeIndexConf', {
+        isFooter: true,
+        isHeader: true,
+        isSearch: false,
+        title: '发布'
+      })
+  },
+  methods:{
+  }
 }
 </script>
-<style>
+<style lang='scss'>
+.app-publish{
+	margin-top:45px;
+}
 </style>
