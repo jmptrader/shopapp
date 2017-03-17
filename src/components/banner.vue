5deefee23f5a0394ad3be093ec000c75019cf0d8
cuixiaoli <cuixiaoli>
2017-03-17 18:12:33 +0800

    vue-awesome-swiper

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -1,18 +1,7 @@
 <template>
-<swiper v-ref:swiper
-        direction="horizontal"
-        :mousewheel-control="true"
-        :performance-mode="false"
-        :pagination-visible="true"
-        :pagination-clickable="true"
-        :loop="true"
-        @slide-change-start="onSlideChangeStart"
-        @slide-change-end="onSlideChangeEnd">
-    <div v-for='item in banner'><img class='app-img' :src="item.src" alt=""></div>
-</swiper>
+
 </template>
 <script>
-import Swiper from 'vue-swiper'
 export default{
 	data(){
 		return {
@@ -20,15 +9,10 @@ export default{
 	},
 	props:['banner'],
 	components:{
-		Swiper
 	},
+    created(){
+    },
 	methods:{
-		onSlideChangeStart (currentPage) {
-            // console.log('onSlideChangeStart', currentPage);
-        },
-        onSlideChangeEnd (currentPage) {
-            // console.log('onSlideChangeEnd', currentPage);
-        }
 	}
 }
 </script>
