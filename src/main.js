8a63915730bd4b9835a6df79a927b076cc058c1c
ever <cui_xl@163.com>
2017-03-18 18:39:33 +0800

    add swiper

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import store from './store'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+Vue.use(VueAwesomeSwiper)
 
 Vue.config.productionTip = false
 
